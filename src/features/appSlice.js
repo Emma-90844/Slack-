e2bb7dd551d7b67daec04c3047312e308987a84a
Emma-90844 <enabojohn@gmail.com>
2021-11-09 12:50:48 +0300

    Added enterRoom action in the appSlice reducer

---
+++ src/features/appSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+
+export const appSlice = createSlice({
+    name: 'app',
+    initialState: {
+       roomId: null,
+ 
+    },
+    reducers:{
+        enterRoom: (state, action) => {
+            state.roomId = action.payload.roomId;
+        }
+    }
+
+});
+
+
+export const { enterRoom } = appSlice.actions;
+// Grabbing from the store uses selector
+// SELECTORS
+export const selectRoomId = state => state.app.roomId;
+  
+
+export default appSlice.reducer;
+
