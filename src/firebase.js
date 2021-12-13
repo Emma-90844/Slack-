94f1754d5ebff129c0590d1b1f96dbb7cea88e24
Emma-90844 <enabojohn@gmail.com>
2021-12-13 18:40:44 +0300

    Added firebase configuration from Firebase Console

--- src/firebase.js
+++ src/firebase.js
@@ -1,31 +1,48 @@
-// import firebase  from '@firebase/compat/app';
-import '@firebase/firestore'
+// // import firebase  from '@firebase/compat/app';
+// import '@firebase/firestore'
 
 
-import firebase from 'firebase'
-// require('firebase/auth')
+// import firebase from 'firebase'
+// // require('firebase/auth')
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAKlHTRAJHUSV3KUQJShOYVVt-qzWJDeEo",
+//   authDomain: "slack-clone-7bc44.firebaseapp.com",
+//   projectId: "slack-clone-7bc44",
+//   storageBucket: "slack-clone-7bc44.appspot.com",
+//   messagingSenderId: "1065027148297",
+//   appId: "1:1065027148297:web:54b7ddefa001ef511b76d8"
+// };
+
+//   const firebaseApp = firebase.initializeApp(firebaseConfig);
+//   const db = firebaseApp.firestore();
+//   const auth = firebase.auth();
+//   const provider = new firebase.auth.GoogleAuthProvider();
+
+
+// export {auth, provider, db}
+import firebase from "firebase";
+import 'firebase/firestore';
+
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBbI2pBSRGzjgxXUOJxUtM3BcE-sreBuP4",
-    authDomain: "slack-clone-28559.firebaseapp.com",
-    projectId: "slack-clone-28559",
-    storageBucket: "slack-clone-28559.appspot.com",
-    messagingSenderId: "234049875172",
-    appId: "1:234049875172:web:6b65ffa9617a24c4c550f1"
-  };
+  apiKey: "AIzaSyCRL544TddX78tKd66DcE6Ftt1XuQTjtCs",
+  authDomain: "slack-clone-f4237.firebaseapp.com",
+  projectId: "slack-clone-f4237",
+  storageBucket: "slack-clone-f4237.appspot.com",
+  messagingSenderId: "573406534144",
+  appId: "1:573406534144:web:d1bb6003d0cd069c8dd6e5",
+  measurementId: "G-PHK4HX2VV8"
+};
 
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
 
-  const firebaseApp = firebase.initializeApp(firebaseConfig);
-  const db = firebaseApp.firestore();
-  const auth = firebase.auth();
-  const provider = new firebase.auth.GoogleAuthProvider();
+const auth = firebase.auth();
+const provider  = new firebase.auth.GoogleAuthProvider();
+ 
 
-
-export {auth, provider, db}
-
-
-
-
+export { auth, provider, db};
 
 
 
